1d3d6c54e58a27c0e4ff1508774f4e156fd24afd
SarthTheSnek <34319569+SarthTheSnek@users.noreply.github.com>
2021-01-14 15:51:14 -0800

    iasdf

--- ffva/__init__.py
+++ ffva/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     app.config['DISCORD_BOT_TOKEN'] = os.getenv('DISCORD_BOT_TOKEN')
 
     from .auth import auth as auth_blueprint, discord
-    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
     discord.init_app(app)
 
     from .main import main as main_blueprint
