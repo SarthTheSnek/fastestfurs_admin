13f33fa730932439141a338ac558cf23916d1146
SarthTheSnek <34319569+SarthTheSnek@users.noreply.github.com>
2021-01-16 15:35:13 -0800

    Better authorization for the site

--- ffva/auth.py
+++ ffva/auth.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, redirect, url_for, render_template, current_app as app
-from flask_discord import requires_authorization, Unauthorized
+import os
+import requests
+from flask import Blueprint, redirect, url_for, session, current_app as app
+from flask_discord import Unauthorized
 
 auth = Blueprint('auth', __name__)
 
@@ -12,7 +14,8 @@ def login():
 @auth.route('/callback')
 def callback():
     app.discord.callback()
-    return redirect(url_for('auth.profile'))
+    check_permissions()
+    return redirect(url_for('main.index'))
 
 
 @auth.errorhandler(Unauthorized)
@@ -23,4 +26,31 @@ def redirect_unauthorized(e):
 @auth.route('/logout')
 def logout():
     app.discord.revoke()
+    session.pop('PERM', None)
     return redirect(url_for('main.index'))
+
+
+def check_permissions():
+    user = app.discord.fetch_user()
+    guilds = app.discord.fetch_guilds()
+    if next((x for x in guilds if x._payload.get('id') == os.getenv('FF_SERVER_ID')), None):
+        session['PERM'] = call_server(user.id)
+    else:
+        session['PERM'] = 1
+    return session
+
+
+def call_server(user) -> int:
+    base_url = "https://discord.com/api"
+    path = f"/guilds/{os.getenv('FF_SERVER_ID')}/members/{user}"
+    token_value = f"Bot {os.getenv('DISCORD_BOT_SECRET')}"
+    headers = {
+            "Authorization": token_value
+            }
+    response = requests.get(base_url + path, headers=headers)
+    if os.getenv('FF_LEADERSHIP_ROLE') in response.json().get('roles'):
+        return 4
+    elif os.getenv('FF_VOLUNTEER_ROLE') in response.json().get('roles'):
+        return 2
+    else:
+        return 1
--- ffva/main.py
+++ ffva/main.py
@@ -1,24 +1,20 @@
-from flask import Blueprint, render_template, redirect, url_for, current_app as app
+from flask import Blueprint, render_template, redirect, url_for, session, current_app as app
 from flask_discord import requires_authorization, Unauthorized
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
+    if session.get('PERM'):
+        user = app.discord.fetch_user()
+    else:
+        user = None
+
     return render_template(
         'index.html',
-        authorized=app.discord.authorized
-    )
-
-
-@main.route('/profile')
-@requires_authorization
-def profile():
-    user = app.discord.fetch_user()
-    return render_template(
-        'profile.html',
-        name=user.name,
-        authorized=app.discord.authorized
+        user=user,
+        authorized=app.discord.authorized,
+        perm_level=session['PERM']
     )
 
 
